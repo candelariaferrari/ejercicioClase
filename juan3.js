6b0155cb30ea680b89c2832f382d2f0cfc045533
juanpampean <juanfernandezborasio@gmail.com>
2021-12-21 21:07:33 -0300

    cambios en Juan3.js

--- juan3.js
+++ juan3.js
@@ -3,4 +3,10 @@ let n2=3
 
 function resta(n1,n2){return n1-n2}
 
-console.log(suma(n1,n2))
+console.log(resta(n1,n2))
+n1=2
+n2=10
+
+function multiplicar(n1,n2){return n1*n2}
+
+console.log(multiplicar(n1,n2))
